8421423b7c7fa6dcf86ed2c5bf02f5876c7c458e
Mike <bmike06@gmail.com>
2020-02-26 10:18:29 -0600

    Update Functions.cs

--- Functions.cs
+++ Functions.cs
@@ -32,6 +32,8 @@ namespace SyncTest
                 
                 messageReceiver.CompleteAsync(message.SystemProperties.LockToken);
                 
+                Upsert(message.PartitionKey); // this call takes over 20 minutes to complete, I have no included this api call in the source, issues are before this is even called
+                
                 logger.Info("Sync ended.");
             }
             catch (Exception e)
